dbe79d54864f67c58fa7c0ba633a56faaf21ac1a
takuro-furukawa <tic.tak.guchuan@gmail.com>
2024-04-03 22:27:11 +0900

    add input

--- src/main/java/org/example/gitrepocontroller/GitRepoControllerApplication.java
+++ src/main/java/org/example/gitrepocontroller/GitRepoControllerApplication.java
@@ -10,6 +10,7 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.example.gitrepocontroller.bean.BranchInfo;
 import org.example.gitrepocontroller.bean.CommitInfo;
 import org.example.gitrepocontroller.bean.GitInfo;
+import org.example.gitrepocontroller.bean.PageInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -48,7 +49,7 @@ class GitRepoController {
      */
     @GetMapping(value = "/")
     public String home(Model model) {
-        model.addAttribute("GitInfo", new GitInfo());
+        model.addAttribute("PageInfo", new PageInfo(new GitInfo(), new GitInfo()));
         log.debug("init page");
         if (!WORKSPACE_PATH.toFile().exists()) {
             WORKSPACE_PATH.toFile().mkdirs();
@@ -58,20 +59,19 @@ class GitRepoController {
     }
 
     /**
-     * get list of branch
+     * get list of branch for repo1
      *
      * @param model
-     * @param gitInfo
+     * @param pageInfo
      * @return
      * @throws IOException
      * @throws GitAPIException
      */
-    @GetMapping(value = "/", params = "repo")
-    public String getBranch(Model model, GitInfo gitInfo) throws IOException, GitAPIException {
-        model.addAttribute("GitInfo", gitInfo);
+    @GetMapping(value = "/", params = "repo1")
+    public String getBranch1(Model model, PageInfo pageInfo) throws IOException, GitAPIException {
         log.debug("get repo");
+        GitInfo gitInfo = pageInfo.getGitInfoFrom();
         Git git = getGit(gitInfo.getRepoUrl());
-        git.checkout().setName("main").call();
         git.pull().setRemote("origin").call();
         List<BranchInfo> branchInfoList = new ArrayList<>();
         git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call().forEach(ref -> {
@@ -79,30 +79,82 @@ class GitRepoController {
         });
         gitInfo.setBranchInfoList(branchInfoList);
 
+        model.addAttribute("PageInfo", pageInfo);
         return "index";
     }
 
     /**
-     * get list of log
+     * get list of log for branch1
      *
      * @param model
-     * @param gitInfo
+     * @param pageInfo
      * @return
      * @throws GitAPIException
      * @throws IOException
      */
-    @GetMapping(value = "/", params = "branch")
-    public String getLogs(Model model, GitInfo gitInfo) throws GitAPIException, IOException {
-        model.addAttribute("GitInfo", gitInfo);
+    @GetMapping(value = "/", params = "branch1")
+    public String getLogs1(Model model, PageInfo pageInfo) throws GitAPIException, IOException {
+        GitInfo gitInfo = pageInfo.getGitInfoFrom();
         log.debug("get branch");
         Git git = getGit(gitInfo.getRepoUrl());
         git.checkout().setName(gitInfo.getBranchName());
         List<CommitInfo> commitInfoList = new ArrayList<>();
         git.log().setMaxCount(10).call().forEach(revCommit -> {
-            commitInfoList.add(new CommitInfo(revCommit.getId().toString(), revCommit.getFullMessage()));
+            commitInfoList.add(new CommitInfo(revCommit.getId().getName(), revCommit.getFullMessage()));
         });
         gitInfo.setCommitInfoList(commitInfoList);
 
+        model.addAttribute("PageInfo", pageInfo);
+        return "index";
+    }
+
+    /**
+     * get list of branch for repo2
+     *
+     * @param model
+     * @param pageInfo
+     * @return
+     * @throws IOException
+     * @throws GitAPIException
+     */
+    @GetMapping(value = "/", params = "repo2")
+    public String getBranch2(Model model, PageInfo pageInfo) throws IOException, GitAPIException {
+        log.debug("get repo");
+        GitInfo gitInfo = pageInfo.getGitInfoTo();
+        Git git = getGit(gitInfo.getRepoUrl());
+        git.pull().setRemote("origin").call();
+        List<BranchInfo> branchInfoList = new ArrayList<>();
+        git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call().forEach(ref -> {
+            branchInfoList.add(new BranchInfo(ref.getName()));
+        });
+        gitInfo.setBranchInfoList(branchInfoList);
+
+        model.addAttribute("PageInfo", pageInfo);
+        return "index";
+    }
+
+    /**
+     * get list of log for branch2
+     *
+     * @param model
+     * @param pageInfo
+     * @return
+     * @throws GitAPIException
+     * @throws IOException
+     */
+    @GetMapping(value = "/", params = "branch2")
+    public String getLogs2(Model model, PageInfo pageInfo) throws GitAPIException, IOException {
+        GitInfo gitInfo = pageInfo.getGitInfoTo();
+        log.debug("get branch");
+        Git git = getGit(gitInfo.getRepoUrl());
+        git.checkout().setName(gitInfo.getBranchName());
+        List<CommitInfo> commitInfoList = new ArrayList<>();
+        git.log().setMaxCount(10).call().forEach(revCommit -> {
+            commitInfoList.add(new CommitInfo(revCommit.getId().getName(), revCommit.getFullMessage()));
+        });
+        gitInfo.setCommitInfoList(commitInfoList);
+
+        model.addAttribute("PageInfo", pageInfo);
         return "index";
     }
 
--- src/main/java/org/example/gitrepocontroller/bean/PageInfo.java
+++ src/main/java/org/example/gitrepocontroller/bean/PageInfo.java
@@ -1,8 +1,12 @@
 package org.example.gitrepocontroller.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageInfo {
     GitInfo gitInfoFrom;
     GitInfo gitInfoTo;
