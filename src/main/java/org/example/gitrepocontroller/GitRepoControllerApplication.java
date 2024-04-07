a35f3635fbd01e0f46521069d3736eb72b021be3
takuro-furukawa <tic.tak.guchuan@gmail.com>
2024-04-07 22:14:14 +0900

    fix push method

--- src/main/java/org/example/gitrepocontroller/GitRepoControllerApplication.java
+++ src/main/java/org/example/gitrepocontroller/GitRepoControllerApplication.java
@@ -2,22 +2,21 @@ package org.example.gitrepocontroller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.errors.IncorrectObjectTypeException;
-import org.eclipse.jgit.errors.MissingObjectException;
-import org.eclipse.jgit.errors.StopWalkException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.revwalk.filter.RevFilter;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.example.gitrepocontroller.bean.BranchInfo;
 import org.example.gitrepocontroller.bean.CommitInfo;
 import org.example.gitrepocontroller.bean.GitInfo;
 import org.example.gitrepocontroller.bean.PageInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -30,7 +29,9 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.StreamSupport;
 
 @SpringBootApplication
@@ -49,6 +50,11 @@ class GitRepoController {
     Log log = LogFactory.getLog("Controller");
     private static final String WORKSPACE = "/tmp/workspace";
     private static final Path WORKSPACE_PATH = Paths.get(WORKSPACE);
+
+    @Value("${git.credential.username}")
+    private String USERNAME;
+    @Value("${git.credential.password}")
+    private String PASSWORD;
 
     /**
      * show first page
@@ -77,10 +83,10 @@ class GitRepoController {
      * @throws GitAPIException
      */
     @GetMapping(value = "/", params = "repo1")
-    public String getBranch1(Model model, PageInfo pageInfo) throws IOException, GitAPIException {
+    public String getBranch1(Model model, PageInfo pageInfo) throws IOException, GitAPIException, URISyntaxException {
         log.debug("get repo");
         GitInfo gitInfo = pageInfo.getGitInfoFrom();
-        Git git = getGit(gitInfo.getRepoUrl());
+        Git git = getGit(gitInfo.getRepoUrl(), true);
         gitInfo.setBranchInfoList(getBranch(git));
 
         model.addAttribute("PageInfo", pageInfo);
@@ -97,10 +103,10 @@ class GitRepoController {
      * @throws IOException
      */
     @GetMapping(value = "/", params = "branch1")
-    public String getLogs1(Model model, PageInfo pageInfo) throws GitAPIException, IOException {
+    public String getLogs1(Model model, PageInfo pageInfo) throws GitAPIException, IOException, URISyntaxException {
         GitInfo gitInfo = pageInfo.getGitInfoFrom();
         log.debug("get branch");
-        Git git = getGit(gitInfo.getRepoUrl());
+        Git git = getGit(gitInfo.getRepoUrl(), true);
         gitInfo.setCommitInfoList(getLog(git, gitInfo.getBranchName()));
 
         model.addAttribute("PageInfo", pageInfo);
@@ -117,10 +123,10 @@ class GitRepoController {
      * @throws GitAPIException
      */
     @GetMapping(value = "/", params = "repo2")
-    public String getBranch2(Model model, PageInfo pageInfo) throws IOException, GitAPIException {
+    public String getBranch2(Model model, PageInfo pageInfo) throws IOException, GitAPIException, URISyntaxException {
         log.debug("get repo");
         GitInfo gitInfo = pageInfo.getGitInfoTo();
-        Git git = getGit(gitInfo.getRepoUrl());
+        Git git = getGit(gitInfo.getRepoUrl(), false);
         gitInfo.setBranchInfoList(getBranch(git));
 
         model.addAttribute("PageInfo", pageInfo);
@@ -137,11 +143,10 @@ class GitRepoController {
      * @throws IOException
      */
     @GetMapping(value = "/", params = "branch2")
-    public String getLogs2(Model model, PageInfo pageInfo) throws GitAPIException, IOException {
+    public String getLogs2(Model model, PageInfo pageInfo) throws GitAPIException, IOException, URISyntaxException {
         GitInfo gitInfo = pageInfo.getGitInfoTo();
         log.debug("get branch");
-        Git git = getGit(gitInfo.getRepoUrl());
-        git.checkout().setName(gitInfo.getBranchName());
+        Git git = getGit(gitInfo.getRepoUrl(), false);
         gitInfo.setCommitInfoList(getLog(git, gitInfo.getBranchName()));
 
         model.addAttribute("PageInfo", pageInfo);
@@ -151,29 +156,42 @@ class GitRepoController {
     @GetMapping(value = "/", params = "cp")
     public String cherryPick(Model model, PageInfo pageInfo) throws GitAPIException, IOException, URISyntaxException {
         GitInfo gitInfoFrom = pageInfo.getGitInfoFrom();
-        Git gitFrom = getGit(gitInfoFrom.getRepoUrl());
+        Git gitFrom = getGit(gitInfoFrom.getRepoUrl(), true);
         gitFrom.checkout().setName(gitInfoFrom.getBranchName()).call();
         gitFrom.fetch().setRemote("origin").call();
         RevFilter filter = RevFilter.NO_MERGES;
         List<RevCommit> pickedCommitList = new ArrayList<>();
-        for (String commitId : gitInfoFrom.getCheckedCommitId()) {
-            RevCommit revCommit = (RevCommit) StreamSupport.stream(gitFrom.log().setRevFilter(filter).call().spliterator(), false)
-                    .filter(rc -> rc.getId().getName().equals(commitId));
-            pickedCommitList.add(revCommit);
-        }
+        Arrays.asList(gitInfoFrom.getCheckedCommitId()).forEach(s -> {
+            try {
+                StreamSupport.stream(gitFrom.log().setRevFilter(filter).call().spliterator(), false)
+                        .filter(rc -> rc.getId().getName().equals(s)).forEach(pickedCommitList::add);
+            } catch (GitAPIException e) {
+                throw new RuntimeException("error while trying to search selected commit", e);
+            }
+        });
 
         GitInfo gitInfoTo = pageInfo.getGitInfoTo();
-        Git gitTo = getGit(gitInfoTo.getRepoUrl());
-        gitTo.remoteAdd().setName("fork").setUri(new URIish(gitInfoFrom.getRepoUrl()));
+        Git gitTo = getGit(gitInfoTo.getRepoUrl(), false);
+        gitTo.remoteAdd().setName("fork").setUri(new URIish(gitInfoFrom.getRepoUrl())).call();
         gitTo.fetch().setRemote("fork").call();
+        gitTo.checkout().setName(gitInfoTo.getBranchName()).call();
+        pickedCommitList.forEach(rc -> {
+            try {
+                gitTo.cherryPick().include(rc.getId()).call();
+            } catch (GitAPIException e) {
+                throw new RuntimeException("error while trying to cherry-pick selected commit", e);
+            }
+        });
+
+        gitTo.push().setRemote("origin").setCredentialsProvider(new UsernamePasswordCredentialsProvider(USERNAME, PASSWORD)).call();
 
         model.addAttribute("PageInfo", pageInfo);
         return "index";
     }
 
-    private Git getGit(String uri) throws GitAPIException, IOException {
+    private Git getGit(String uri, boolean isFrom) throws GitAPIException, IOException, URISyntaxException {
         String repoName = Paths.get(uri).getFileName().toString();
-        Path repoPath = Paths.get(WORKSPACE + "/" + repoName);
+        Path repoPath = Paths.get(WORKSPACE + "/" + repoName + isFrom);
         Git git;
         if (!repoPath.toFile().exists()) {
             repoPath.toFile().mkdirs();
@@ -183,14 +201,25 @@ class GitRepoController {
             Repository repo = new FileRepositoryBuilder().setGitDir(path.toFile()).build();
             git = new Git(repo);
         }
+        git.remoteAdd().setName("origin").setUri(new URIish(uri)).call();
         return git;
     }
 
     private List<CommitInfo> getLog(Git git, String branchName) throws GitAPIException {
-        git.checkout().setName(branchName).call();
         git.fetch().setRemote("origin").call();
+        AtomicBoolean createBranch = new AtomicBoolean(true);
+        git.branchList().call()
+                .stream()
+                .filter(ref -> ref.getName().contains(branchName))
+                        .forEach(ref -> createBranch.set(false));
+        git.checkout()
+                .setCreateBranch(createBranch.get())
+                .setName(branchName)
+                .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK)
+                .setStartPoint("origin/" + branchName)
+                .call();
         List<CommitInfo> commitInfoList = new ArrayList<>();
-        git.log().setMaxCount(10).call().forEach(revCommit -> {
+        git.log().setRevFilter(RevFilter.NO_MERGES).setMaxCount(10).call().forEach(revCommit -> {
             commitInfoList.add(new CommitInfo(revCommit.getId().getName(), revCommit.getFullMessage()));
         });
         return commitInfoList;
@@ -199,9 +228,9 @@ class GitRepoController {
     private List<BranchInfo> getBranch(Git git) throws GitAPIException {
         git.fetch().setRemote("origin").call();
         List<BranchInfo> branchInfoList = new ArrayList<>();
-        git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call().forEach(ref -> {
-            branchInfoList.add(new BranchInfo(ref.getName()));
-        });
+        git.branchList().setListMode(ListBranchCommand.ListMode.REMOTE).call()
+                .stream().filter(ref -> ref.getName().contains("origin"))
+                .forEach(ref -> branchInfoList.add(new BranchInfo(ref.getName().replaceFirst("refs/remotes/origin/", ""))));
         return branchInfoList;
     }
 }
